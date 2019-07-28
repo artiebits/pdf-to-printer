53685d38dcf2bccf9e9f4e9559e71426554861cb
artiebits <artur.khusaenov@gmail.com>
2019-07-28 12:46:36 +0200

    fix the path to SumatraPDF.exe for Electron (#10)

---
+++ src/electron-util.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Taken from https://github.com/sindresorhus/electron-util/blob/master/node.js
+
+const isElectron = "electron" in process.versions;
+
+const isUsingAsar =
+  isElectron &&
+  process.mainModule &&
+  process.mainModule.filename.includes("app.asar");
+
+export const fixPathForAsarUnpack = path => {
+  return isUsingAsar ? path.replace("app.asar", "app.asar.unpacked") : path;
+};
