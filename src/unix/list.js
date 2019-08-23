fdbea8738ec8efdeb40e12498cf2835ca5cb9f09
Sveta Khannanova <ifrodia@gmail.com>
2019-08-23 22:08:13 +0200

    delete console.log

--- src/unix/list.js
+++ src/unix/list.js
@@ -5,12 +5,10 @@ const execAsync = require("../execAsync");
 const list = () => {
   const command = "lpstat -a";
   const stdoutHandler = stdout => {
-    const result = stdout
+    return stdout
       .trim()
       .split("\n")
       .map(e => e.substr(0, e.indexOf(" ")));
-    console.log(result);
-    return result;
   };
 
   return execAsync(command, stdoutHandler);
