22c1b7ba9ed45c2d91f108c7dbd50a3f74045acb
artiebits <artur.khusaenov@gmail.com>
2019-09-28 12:03:35 +0300

    Fix lint error

--- src/unix/list.js
+++ src/unix/list.js
@@ -2,7 +2,11 @@
 
 const execAsync = require("../execAsync");
 
-const stdoutHandler = stdout => stdout.trim().split("\n").map(e => e.substr(0, e.indexOf(" ")));
+const stdoutHandler = stdout =>
+  stdout
+    .trim()
+    .split("\n")
+    .map(e => e.substr(0, e.indexOf(" ")));
 
 const list = () => execAsync("lpstat", ["-a"], stdoutHandler);
 
