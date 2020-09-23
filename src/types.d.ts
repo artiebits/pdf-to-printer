cfc37a5261181ca9c0f68518b7bb44214ee33541
Matyx <matyx@users.noreply.github.com>
2020-09-23 11:31:02 +0200

    Add type definitions for typescript (#191)

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+export interface Options {
+    printer?: string;
+    unix?: string[];
+    win32?: string[];
+}
+
+export function print(path: string, options?: any): Promise<void>;
+
+export function getPrinters(): Promise<string[]>;
+
+export function getDefaultPrinter(): Promise<string>;
