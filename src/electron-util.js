6e43eb7c005c341e40eda4dc96e04c71ed1aff7f
David Gasperoni <mcdado@gmail.com>
2021-02-09 18:13:56 +0100

    Update electron-util
    Use exports to allow real-time debugging of unbuilt source

--- src/electron-util.js
+++ src/electron-util.js
@@ -9,6 +9,6 @@ const isUsingAsar =
   process.mainModule &&
   process.mainModule.filename.includes("app.asar");
 
-export const fixPathForAsarUnpack = (path) => {
+exports.fixPathForAsarUnpack = (path) => {
   return isUsingAsar ? path.replace("app.asar", "app.asar.unpacked") : path;
 };
