ac8a8c0af191fd588335946feaa7a4970da2bcdf
Andrei Rădoi <andreiradoidev@gmail.com>
2021-04-12 14:11:55 +0300

    Changed name to deviceId and displayName to name

--- src/win32/get-printers.js
+++ src/win32/get-printers.js
@@ -8,15 +8,18 @@ const getPrinters = () => {
       .trim()
       .split(/[\r\n]+/)
       .map((line) => line.trim().split(/\s{2,}/));
+
     const headers = result[0].reduce((acc, curr, index) => {
       acc[curr] = index;
       return acc;
     }, {});
+
     return result.slice(1).map((printerData) => ({
-      name: printerData[headers["DeviceID"]],
-      displayName: printerData[headers["Name"]],
+      deviceId: printerData[headers["DeviceID"]],
+      name: printerData[headers["Name"]],
     }));
   };
+
   // https://ss64.com/nt/wmic.html#alias_options
   return execAsync("wmic", ["printer", "get", "deviceid,name"], stdoutHandler);
 };
