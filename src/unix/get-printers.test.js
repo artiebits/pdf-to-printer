fc36cda7072e9e4da471fb59a5b03fdf7acd487d
Andrei Rădoi <andreiradoidev@gmail.com>
2021-04-12 14:13:51 +0300

    Updated tests to reflect changes

--- src/unix/get-printers.test.js
+++ src/unix/get-printers.test.js
@@ -50,9 +50,31 @@ Default page size:
 Default port settings:
 `;
 
-const mockDefaultPrinterStdout = `
-system default destination: EPSON
-    `;
+const mockDefaultPrinterStdout = `system default destination: Virtual_PDF_Printer
+`;
+
+const mockDefaultPrinterDataStdout = `printer Virtual_PDF_Printer is idle.  enabled since Mon 12 Apr 2021 01:35:20 PM EEST
+Form mounted:
+Content types: any
+Printer types: unknown
+Description: Virtual PDF Printer
+Alerts: none
+Location:
+Connection: direct
+Interface: /etc/cups/ppd/Virtual_PDF_Printer.ppd
+On fault: no alert
+After fault: continue
+Users allowed:
+        (all)
+Forms allowed:
+        (none)
+Banner required
+Charset sets:
+        (none)
+Default pitch:
+Default page size:
+Default port settings:
+`;
 
 afterEach(() => {
   // restore the original implementation
@@ -63,26 +85,44 @@ test("returns list of available printers", () => {
   execAsync.mockImplementation((_, [], callback) =>
     Promise.resolve(callback(mockPrinterListStdout))
   );
+
   return expect(getPrinters()).resolves.toStrictEqual([
     {
-      name: "Virtual_Braille_BRF_Printer",
-      displayName: "Virtual Braille BRF Printer",
+      deviceId: "Virtual_Braille_BRF_Printer",
+      name: "Virtual Braille BRF Printer",
     },
     {
-      name: "Virtual_PDF_Printer",
-      displayName: "Virtual PDF Printer",
+      deviceId: "Virtual_PDF_Printer",
+      name: "Virtual PDF Printer",
     },
   ]);
 });
 
 test("returns the system default printer", () => {
+  execAsync
+    .mockImplementationOnce((_, [], callback) =>
+      Promise.resolve(callback(mockDefaultPrinterStdout))
+    )
+    .mockImplementationOnce((_, [], callback) =>
+      Promise.resolve(callback(mockDefaultPrinterDataStdout))
+    );
+
+  return expect(getDefaultPrinter()).resolves.toStrictEqual({
+    deviceId: "Virtual_PDF_Printer",
+    name: "Virtual PDF Printer",
+  });
+});
+
+test("test no default printer defined", () => {
   execAsync.mockImplementation((_, [], callback) =>
-    Promise.resolve(callback(mockDefaultPrinterStdout))
+    Promise.resolve(callback("no system default destination"))
   );
-  return expect(getDefaultPrinter()).resolves.toStrictEqual("EPSON");
+
+  return expect(getDefaultPrinter()).resolves.toStrictEqual(false);
 });
 
 test("fails with an error", () => {
   execAsync.mockImplementation(() => Promise.reject("error"));
+
   return expect(getPrinters()).rejects.toBe("error");
 });
