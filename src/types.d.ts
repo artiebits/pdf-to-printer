b456c0c11caff8ca7863cd4228a938f27d6b61f4
Andrei Rădoi <andreiradoidev@gmail.com>
2021-04-17 10:32:11 +0300

    Updated type declarations (#244)

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,8 +4,13 @@ export interface Options {
   win32?: string[];
 }
 
+export interface PrinterData {
+  deviceId: string;
+  name: string;
+}
+
 export function print(path: string, options?: any): Promise<void>;
 
-export function getPrinters(): Promise<string[]>;
+export function getPrinters(): Promise<PrinterData[]>;
 
-export function getDefaultPrinter(): Promise<string>;
+export function getDefaultPrinter(): Promise<PrinterData> | false;
