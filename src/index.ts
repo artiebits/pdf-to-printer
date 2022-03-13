f96a82fecb116f79cfdebbcee189c783ca0ee06a
artiebits <artur.khusaenov@gmail.com>
2022-03-13 20:53:34 +0100

    feat: export Printer and PrintOptions types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as print } from "./print/print";
 export { default as getPrinters } from "./get-printers/get-printers";
 export { default as getDefaultPrinter } from "./get-default-printer/get-default-printer";
+export { PrintOptions } from "./print/print";
+export { Printer } from "./get-default-printer/get-default-printer";
