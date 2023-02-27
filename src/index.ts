1bc047227260a40d6bdc3945902e362b31240428
smallsoft-gustavo <desenvolvimento3@smallsoft.com.br>
2023-02-27 17:34:26 -0300

    fix(index): fixed paperSizes in printer type; export const properties

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,10 @@ export { default as getPrinters } from "./get-printers/get-printers";
 export { default as getDefaultPrinter } from "./get-default-printer/get-default-printer";
 export { PrintOptions } from "./print/print";
 
-export type Printer = { deviceId: string; name: string } & Partial<{
-  printerPaperNames: string[];
-}>;
+export type Printer = {
+  deviceId: string;
+  name: string;
+  paperSizes: string[];
+};
 
-export const defaultProperties: (keyof Printer)[] = [
-  "deviceId",
-  "name",
-  "printerPaperNames",
-];
+export const properties: (keyof Printer)[] = ["deviceId", "name", "paperSizes"];
